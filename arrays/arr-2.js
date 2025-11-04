63730f7800a0dc073e042adb6fe640ad6bc4b468
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-04 23:45:33 +0530

    reduce()

--- arrays/arr-2.js
+++ arrays/arr-2.js
@@ -7,3 +7,23 @@ console.log(array)
 const array2 = ['cat', 'dog', 'cow']
 array2.pop()
 console.log(array2)
+
+console.log('reduce')
+// .reduce()
+
+let fruits = ['apple', 'mango', 'graps', 'apple', 'graps', 'apple', 'graps', 'mango']
+
+let sum = fruits.reduce((accum , current) => {
+    let apple = ''
+    let mango = ''
+    let graps = ''
+    if(accum === 'apple' && current === 'apple'){
+        return apple++
+    }else if(accum === 'mango' && current === 'mango'){
+        return mango++
+    }else{
+        graps++
+    }
+})
+
+console.log(sum)
