870889370b96510729da8e1b8090f4acf5d085a0
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-12 18:37:27 +0530

    loops complexity

--- Loops/loop-1.js
+++ Loops/loop-1.js
@@ -2,3 +2,11 @@
 for (let i = 0, j = 10; i < j; i++, j--) {
   console.log(i, j);
 }
+
+console.log('complex uses')
+
+let arr = [1 , 2, 3, 4]
+
+for(let sum = arr.reduce((a, b) => a + b, 0); sum > 0 ; sum--) {
+    console.log(sum)
+}
